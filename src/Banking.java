f8d8c45a8fa24c43dcabd949c6df8d41a9864b34
ok-noob <sdfg40768@gmail.com>
2026-08-06 14:36:55 +0500

    Clean up

--- src/Banking.java
+++
@@ -1,7 +0,0 @@
-
-public class Banking {
-    public static void main(String[] args) {
-        int[] number = {57,96,35,84,9,14};
-
-    }
-}
--- src/test/Cat.java
+++
@@ -1,4 +0,0 @@
-package test;
-
-public class Cat extends Animal {
-}
