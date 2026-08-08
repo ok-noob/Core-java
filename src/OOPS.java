46843582dd4b1c3f5fa05d73125eadec04e90974
ok-noob <sdfg40768@gmail.com>
2026-08-08 10:36:24 +0500

    Adding constructor

--- src/OOPS.java
+++ src/OOPS.java
@@ -3,34 +3,39 @@ class Human {
     int age;
     String gender;
 
-    public void eat() {
-        System.out.println(name+" is eating");
-    }
-    public void sleep() {
-        System.out.println(name+" is sleeping");
-    }
-    public void wakeUp() {
-        System.out.println(name+" woke up");
-    }
-    public void getAge(){
-        System.out.println(name+" is "+age+" year old");
-    }
-    public void gender(){
-        System.out.println(name+" gender is "+gender);
-    }
-    public void getName(){
-        System.out.println("Name: "+name);
+    public Human(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
     }
 
-}
+        public void eat () {
+            System.out.println(name + " is eating");
+        }
+        public void sleep () {
+            System.out.println(name + " is sleeping");
+        }
+        public void wakeUp () {
+            System.out.println(name + " woke up");
+        }
+        public void getAge () {
+            System.out.println(name + " is " + age + " year old");
+        }
+        public void gender () {
+            System.out.println(name + " gender is " + gender);
+        }
+        public void getName () {
+            System.out.println("Name: " + name);
+        }
+
+    }
+
 
 public class OOPS {
     public static void main(String args[]) {
 //    Project1
-        Human human1 = new Human();
-        human1.name = "Alex";
-        human1.age = 16;
-        human1.gender = "Male";
+        Human human1 = new Human("Alex", 16,"Male");
+
 //    Methods
         human1.getName();
         human1.getAge();
@@ -38,10 +43,8 @@ public class OOPS {
         System.out.println();
 
 //    Project2
-        Human human2 = new Human();
-        human2.name = "Jane";
-        human2.age = 20;
-        human2.gender = "Female";
+        Human human2 = new Human("Jane",20,"Female");
+
 //    Methods
         human2.getName();
         human2.gender();
