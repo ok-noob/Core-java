30b7b9220d1a76ed6b9d568c46a29fbe7d0e68f7
ok-noob <sdfg40768@gmail.com>
2026-08-20 11:12:12 +0500

    encasulation

---
+++ src/opps/Book.java
@@ -0,0 +1,35 @@
+package opps;
+
+public class Book {
+  private   String author;
+   private String title;
+   private int pageCount;
+
+    Book(String author, String title, int pageCount) {
+        this.author = author;
+        this.title = title;
+        this.pageCount = pageCount;
+    }
+
+    //    Getter
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        if (pageCount < 10)
+            throw new IllegalStateException("A book must have at least 10 pages!");
+        else {
+            this.pageCount = pageCount;
+            System.out.println("Page is updated to " + pageCount + "!");
+        }
+    }
+}
---
+++ src/opps/bookrun.java
@@ -0,0 +1,10 @@
+package opps;
+
+public class bookrun {
+    public static void main(String[] args) {
+
+    Book b1 = new Book("Goggins","Can't hurt me", 20);
+    b1.setPageCount(35);
+
+}
+}
